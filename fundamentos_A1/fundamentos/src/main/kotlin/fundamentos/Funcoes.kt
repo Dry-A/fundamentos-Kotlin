7c6056ec09a8d1ccb82f19478183094bebd6ea52
Dry-A <112403510+Dry-A@users.noreply.github.com>
2025-03-03 14:53:17 -0300

    :bee: complemento da classe funções e exclusao de outro projeto

--- fundamentos_A1/fundamentos/src/main/kotlin/fundamentos/Funcoes.kt
+++ fundamentos_A1/fundamentos/src/main/kotlin/fundamentos/Funcoes.kt
@@ -1,11 +1,12 @@
 package fundamentos
 
-fun main(){
-    var nome = mostraNome()!!
-    println(nome)
-    dizOi(nome)
-    dizOi("Romilda")
-
+fun main() {
+    parImpar(10)
+    parImpar(11)
+    resultadoNota(7.6)
+    resultadoNota(4.9)
+    resultadoNota(5.9)
+    resultadoNota(9.7)
 }
 
 fun mostraNome(): String {
@@ -13,6 +14,25 @@ fun mostraNome(): String {
 
 }
 
-fun dizOi(nome: String, idade: Int = 18){
+fun dizOi(nome: String, idade: Int = 18) {
     println("Oi $nome, é um prazer tê-lo aqui!, você tem $idade anos ou mais?")
+}
+
+fun parImpar(numero: Int) {
+
+    if (numero % 2 == 0) {
+        println("Par")
+    } else {
+        println("Ímpar")
+    }
+}
+
+fun resultadoNota(nota: Double) {
+    if(nota < 5){
+        println("Reprovado")
+    } else if(nota >= 5 && nota < 7){
+        println("Em recuperação")
+    } else {
+        println("Aprovado")
+    }
 }
